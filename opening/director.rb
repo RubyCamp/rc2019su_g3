b4d61f8d152c54f9ed88002fbcac056d4fabee56
akilawww <kd1205710@st.kobedenshi.ac.jp>
2019-08-22 17:06:53 +0900

    ゲームのリトライ時に点数が保持したままなのを直した

--- opening/director.rb
+++ opening/director.rb
@@ -15,6 +15,8 @@ module Opening
         end
 
         def play
+            Scene.set_val(:clear,0)
+            Scene.set_val(:miss,0)
             Scene.move_to(:gaming) if Input.key_push?(K_RETURN) 
             Window.draw_font(170, 10, "しまねあつめ", @font, color:C_GREEN)
             Window.draw_ex(150, 300, @ch_image, scale_x:0.7, scale_y:0.7, center_x:10,center_y:10, angle:-15)      
