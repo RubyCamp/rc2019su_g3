ef7e339ba11c06988fcd154e4098b5667a01b15b
ShuyaKawai <shuya7500@gmail.com>
2019-08-22 15:20:08 +0900

    落下物をランダムに変更

--- gaming/fall.rb
+++ gaming/fall.rb
@@ -10,7 +10,10 @@ end
 class Sprite_shimane < Sprite_sanin
     def initialize(x, y,image_file,director)
         #@str = ["松江", "浜田", "出雲", "益田", "大田", "安来", "江津", "雲南"].sample
-        image = Image.load("images/shimane_moji/matsue.png")
+
+        chose = ["images/shimane_moji/gotsu.png", "images/shimane_moji/hamada.png", "images/shimane_moji/iwami.png", "images/shimane_moji/izumo.png", "images/shimane_moji/masuda.png", "images/shimane_moji/matsue.png", "images/shimane_moji/ohta.png", "images/shimane_moji/unnan.png", "images/shimane_moji/yasugi.png"].sample
+
+        image = Image.load(chose)
         #@font = Font.new(40)
         @yy = rand(5) + 2
         super(x,y,image,director)
@@ -35,7 +38,10 @@ end
 
 class Sprite_tottori < Sprite_sanin
     def initialize(x, y,image_file,director)
-        image = Image.load("images/tottori_moji/aimi.png")
+
+        chose = ["images/tottori_moji/aimi.png", "images/tottori_moji/daiei.png", "images/tottori_moji/iwami.png", "images/tottori_moji/kokufu.png", "images/tottori_moji/kurayoshi.png", "images/tottori_moji/sakaiminato.png", "images/tottori_moji/tottori.png", "images/tottori_moji/yonago.png"].sample
+
+        image = Image.load(chose)
         @yy = rand(5) + 2
         super(x,y,image,director)
     end
