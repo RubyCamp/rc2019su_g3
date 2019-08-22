51c85364161272cefb3eb1cb4dae11c377b47502
nishikawa1031 <tasunishi1031@yahoo.co.jp>
2019-08-22 10:05:58 +0900

    openingの微修正

--- opening/director.rb
+++ opening/director.rb
@@ -6,7 +6,6 @@ module Opening
             @args = args
             prepare_scene
             @bg_image = Image.load('images/simane_bg.jpg')
-            #　https://www.sozai-library.com/　から持ってきた。
             @ch_image = Image.load('images/IMG_7974.png')
         end
         
@@ -16,9 +15,7 @@ module Opening
         end
 
         def play
-            Scene.move_to(:gaming) if Input.key_push?(K_RETURN)
-            # Window.draw(0, 0, @bg_image)    
-
+            Scene.move_to(:gaming) if Input.key_push?(K_RETURN) 
             Window.draw_font(170, 10, "しまねあつめ", @font, color:C_GREEN)
             Window.draw_ex(150, 300, @ch_image, scale_x:0.35, scale_y:0.35, center_x:10,center_y:10, angle:-15)      
             Window.draw_scale(280, 230, @bg_image, 1, 1, 10,10) 
